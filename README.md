02f4c4f10f598bcf3e74d4993fa0ce7df3ca6f88
Marco Uberti <ube.labs@gmail.com>
2015-07-30 15:18:27 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Android Studio NDK Test
+JNI call from Android Studio
+
+- Gradle 2.5
+- JDK 7
+- Android Gradle Plugin experimental 0.1.0
+- Android Studio 1.3.0
+- Attention to the difference in Gradle syntax
---
+++ app/src/main/java/ubelab/com/ndktest/MainActivity.java
@@ -0,0 +1,40 @@
+package ubelab.com.ndktest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        /* Create a TextView and set its content.
+         * the text is retrieved by calling a native
+         * function.
+         */
+        TextView tv = new TextView(this);
+        tv.setText(stringFromJNI());
+        setContentView(tv);
+    }
+
+    /* A native method that is implemented by the
+     * 'hello-jni' native library, which is packaged
+     * with this application.
+     */
+    public native String  stringFromJNI();
+
+    /* this is used to load the 'hello-jni' library on application
+     * startup. The library has already been unpacked into
+     * /data/data/com.example.hellojni/lib/libhello-jni.so at
+     * installation time by the package manager.
+     */
+    static {
+        System.loadLibrary("hello-jni");
+    }
+}
